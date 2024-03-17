94b7111e3b2fd7b4e340a78ffdf57c6d9ae056ff
PsyArd <taranova.sa@mail.ru>
2024-03-17 18:59:02 +0300

    dz1

---
+++ main/java/ru/geekbrains/oop/lesson4/homework/Apple.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.oop.lesson4.homework;
+
+public class Apple extends Fruit {
+    @Override
+    float getWeight() {
+        return 1.0f;
+    }
+}
---
+++ main/java/ru/geekbrains/oop/lesson4/homework/Fruit.java
@@ -0,0 +1,5 @@
+package ru.geekbrains.oop.lesson4.homework;
+
+public abstract class Fruit {
+    abstract float getWeight();
+}
